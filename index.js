c10a0c754ae2790aca0ed54ced907a14d95b2a29
AllisonAnz <AllisonAnzalone@gmail.com>
2021-05-02 15:00:36 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
+
+//Make a call to the function so that it works on the webpage
+addingEventListener()
